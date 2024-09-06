487b054f32391b733534c50b62d128911e6ed5c8
mr.webstrom <112272847+Saahil2710@users.noreply.github.com>
2024-09-06 12:29:30 +0530

    Solved palindrome.js

--- week-2/week-2-js/medium/palindrome.js
+++ week-2/week-2-js/medium/palindrome.js
@@ -1,10 +1,12 @@
 /*
-  Implement a function `isPalindrome` which takes a string as argument and returns true/false as its result.
-  Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
+  Implement a function `isPalindrome` which takes a string as an argument and returns true/false as its result.
+  Note: the input string is case-insensitive which means 'Nan' is a palindrome as 'N' and 'n' are considered case-insensitive.
 */
 
-function isPalindrome(str) {
-  return true;
-}
-
-module.exports = isPalindrome;
+  const str =  "racecar";
+  function palindrome(string){
+    const reverseString = string.split('').reverse().join('');   //reverse a string and check if the string matches the original the string is palindrome 
+    return string === reversedString;
+  }
+  const result = palindrome(string);
+  console.log(result);
